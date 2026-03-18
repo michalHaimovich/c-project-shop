47354525635be85f7f8e6961f912a1accbe38f6d
הנדסת תוכנה <huser@sem.local>
2026-03-18 13:18:45 +0200

    add dal xml

---
+++ DotNet2025_9295_6254/ClassLibrary1/Config.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dal
+{
+    internal static class Config
+    {
+       
+        private  static string fileName = "data-config";
+            static XElement dataConfig=XElement.Load(fileName);
+         
+        private static int productId;
+
+        public static int GetProductId
+        {
+            get {
+                int currentProId = int.Parse(dataConfig.Element("ProductNum").Value); 
+                dataConfig.Element("ProductNum").SetValue((currentProId+1).ToString());
+                dataConfig.Save(fileName);
+                return currentProId;
+                }
+
+           
+        }
+
+
+        private static int saleId;
+
+        public static int GetSaleNum
+        {
+            get
+            {
+                int currentSaleId = int.Parse(dataConfig.Element("SaleNum").Value);
+                dataConfig.Element("SaleNum").SetValue((currentSaleId + 1).ToString());
+                dataConfig.Save(fileName);
+                return currentSaleId;
+            }
+
+
+        }
+    }
+}
---
+++ DotNet2025_9295_6254/ClassLibrary1/SaleImplementation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using DalApi;
+using DO;
+
+namespace Dal
+{
+    internal class SaleImplementation : ISale
+    {
+      
+        private static string fileName = "sales";
+        private string ID = "id";
+        private string PRODUCTID = "product_id";
+        private string AMOUNTTOSALE = "amount_to_sale";
+        private string COUNTTOSALE = "count_to_sale";
+        private string TOCLUB = "to_club";
+        private string STARTDATE = "start_date";
+        private string ENDDATE = "end_date";
+
+
+        private XElement sales = XElement.Load(fileName);
+
+        public Sale Read(int id)
+        {
+            
+        }
+
+        public Sale Read(Func<Sale, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Sale> ReadAll(Func<Sale, bool>? filter = null)
+        {
+            return new List<Sale>();
+        }
+
+        public int Create(Sale sale)
+        {
+            return 0;
+        }
+
+        public void Delete(int id)
+        {
+
+        } 
+
+        public void Update(Sale item)
+        {
+
+        }
+
+    }
+}
